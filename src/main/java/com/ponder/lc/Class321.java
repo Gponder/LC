6100ce85931909940c1313507cb9f80afbe9a8f4
gaoyaqin <yaqin-gao@foxmail.com>
2020-12-08 11:32:06 +0800

    c

--- src/main/java/com/ponder/lc/Class321.java
+++ src/main/java/com/ponder/lc/Class321.java
@@ -53,41 +53,92 @@ public class Class321 {
      * @return
      */
     public int[] maxNumber(int[] nums1, int[] nums2, int k) {
-        choseMaxArr(nums1,nums2);
         return nums1;
     }
 
-    private void choseMaxArr(int[] nums1, int[] nums2){
-        int i = 0;
-        int[] tmp = nums2;
-        while (i<nums1.length && i<nums2.length){
-            if (findMaxNum(nums1,i)<findMaxNum(nums2,i)){
-                nums2 = nums1;
-                nums1 = tmp;
-                return;
+    private void findMaxNum(int[] nums1,int start1,int[] nums2,int start2,int[] result,int index){
+        int temp = nums1[start1];
+        boolean flag;
+        for (int i=start1;i<nums1.length;i++){
+            if (temp<nums1[i]){
+                temp = nums1[i];
+                flag = true;
+                start1 = i;
             }
-            i++;
         }
-        if (nums1.length<nums2.length){
-            nums2 = nums1;
-            nums1 = tmp;
+        for (int j=start2;j<nums2.length;j++){
+            if (temp<nums2[j]){
+                temp = nums2[j];
+                flag = false;
+                start2 = j;
+            }
+        }
+        if (nums1.length-start1+nums2.length-start2 >= result.length - index){
+            result[index] = temp;
+            findMaxNum(nums1,start1,nums2,start2,result,index++);
+        }else{
+
         }
     }
 
-    private int findMaxNum(int[] arr,int start){
-        int max = arr[start];
-        for (int i=start+1;i<arr.length;i++){
-            if (max<arr[i]){
-                max = arr[i];
+    /**
+     *
+     * @param arr
+     * @param index  开始下表
+     * @return
+     */
+    private int getMaxIndex(int[] arr,int index){
+        int temp=arr[index];
+        for (int i = index;i<arr.length;i++){
+            if (temp<arr[i]){
+                temp = arr[i];
+                index = i;
             }
         }
-        return max;
+        return index;
+    }
+
+    /**
+     *
+     * @param nums1
+     * @param nums2
+     * @param k     剩余长度
+     * @param start1    数组1开始位置
+     * @param start2    数组2开始位置
+     */
+    private void un(int[] nums1, int[] nums2, int k,int[] result,int start1,int start2){
+        int indexA = getMaxIndex(nums1, start1);
+        int indexB = getMaxIndex(nums2, start2);
+        if (nums1[indexA]>=nums2[indexB]){
+            if (nums1.length-indexA+nums2.length-start2>=0){
+                result[result.length-k]=nums1[indexA];
+                un(nums1,nums2,k-1,result,indexA,start2);
+            }else {
+                int[] nums11 = Arrays.copyOf(nums1, nums1.length);
+                nums11[indexA] = -1;
+                un(nums11,nums2,k,result,start1,start2);
+            }
+        }
+        if ((nums1[indexA]<=nums2[indexB])){
+            if (nums1.length-indexA+nums2.length-start1>=0){
+                result[result.length-k]=nums2[indexB];
+                un(nums1,nums2,k-1,result,start1,indexB);
+            }else {
+                int[] nums22 = Arrays.copyOf(nums2,nums2.length);
+                nums22[indexB] = -1;
+                un(nums1,nums22,k,result,start1,start2);
+            }
+        }
     }
 
     public static void main(String[] args) {
+        Class321 class321 = new Class321();
         int[] nums1 = new int[]{3, 4, 6, 5};
         int[] nums2 = new int[]{9, 1, 2, 5, 8, 3};
-        int[] arr = new Class321().maxNumber(nums1, nums2, 5);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = class321.maxNumber(nums1, nums2, 5);
+        int index = class321.getMaxIndex(nums1,0);
+        System.out.println(index);
+        System.out.println(arr[index]);
+
     }
 }
